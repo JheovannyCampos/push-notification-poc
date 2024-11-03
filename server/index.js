1fa08d7395809acd4b36f6663c58b89d8a7be2ce
JheovannyCampos <jheovanny06@hotmail.com>
2024-11-02 23:42:57 -0300

    cors

--- server/index.js
+++ server/index.js
@@ -5,10 +5,7 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://push-notification-poc-chi.vercel.app/*",
-  ],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
