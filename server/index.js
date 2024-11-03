0f089ae3d57f235b00a44d37fd197b2ee2ccbffe
JheovannyCampos <jheovanny06@hotmail.com>
2024-11-02 23:56:41 -0300

    cors

--- server/index.js
+++ server/index.js
@@ -4,14 +4,13 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 const vapidKeys = {
   subject: "mailto:jheovannycampos@gmail.com",
