70458d7767afd0a3d03d9c10689e8a91fa0ec539
JheovannyCampos <jheovanny06@hotmail.com>
2024-11-02 23:39:24 -0300

    fix: cors error

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://push-notification-server-one.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
@@ -34,7 +37,7 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.status(200).json("Hello World");
 });
 
 app.post("/subscribe", (req, res) => {
